60098a18c4840255905c3905488b87d8634814bd
VDSMath <matheusvdsdf@gmail.com>
2018-05-15 19:53:40 -0300

    Componente Imagem

--- openReport/src/openreport/PDFtest.java
+++ openReport/src/openreport/PDFtest.java
@@ -18,8 +18,9 @@ public class PDFtest {
         c_Title tb = new c_Title("HISTÓRICO ESCOLAR DE TRANSFERÊNCIA", null);
         c_Header tc = new c_Header("INSTITUTO DE APLICAÇÃO FERNANDO RODRIGUES",
                 "RUA SANTA ALEXANDRINA","50","2278-600",
-                "(21) 2441-5312","www.cap.uerj.br","resources/images/gaara.png");
+                "(21) 2441-5312","www.cap.uerj.br","resources/images/sample.png");
         c_Signature td = new c_Signature("Jorge da Silva Bastos", "Coordenador Adjunto");
+        C_Table te = new C_Table(3,3,"a","b","c","d","e","f","g","h","i");
         
         Document doc = new Document();
         PdfWriter.getInstance(doc, new FileOutputStream(DEST));
@@ -27,6 +28,7 @@ public class PDFtest {
         doc.open();       
         tc.print(doc);
         tb.print(doc);
+        te.print(doc);
         ta.print(doc);
         td.print(doc);
         doc.close();
---
+++ openReport/src/pdfexport/C_Table.java
@@ -0,0 +1,45 @@
+package pdfexport;
+
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class C_Table extends Component{
+    public ArrayList<String> content;
+    public int columnAmount;
+    public int cellPerColumn;
+     
+    public C_Table(int _columnAmount, int _cellPerColumn, String... _content){
+        content = new ArrayList<String>();
+        for(String c : _content){
+             content.add(c);
+        }
+        columnAmount = _columnAmount;
+        cellPerColumn = _cellPerColumn;
+     }
+    @Override
+    public void print(Document document) throws IOException, DocumentException {        
+        
+        PdfPTable table =  new PdfPTable(columnAmount);
+        table.getDefaultCell().setBorder(PdfPCell.BOTTOM | PdfPCell.TOP | PdfPCell.RIGHT | PdfPCell.LEFT);
+        table.getDefaultCell().setPadding(0);
+        
+        PdfPTable subtable;
+        int count = 0;
+        for(int i = 0; i < columnAmount; i++){
+            subtable =  new PdfPTable(1);
+            subtable.getDefaultCell().setBorder(PdfPCell.BOTTOM);
+            for(int j = 0; j < cellPerColumn; j++){
+                if(count < content.size())
+                    subtable.addCell(content.get(count));
+                count++;
+            }
+            table.addCell((subtable));
+        }
+        
+        document.add(table);
+        document.add(new Paragraph("\n"));
+    }
+}
