3ff0a3d5e7382616e178d3166f3e04b80ce756ea
TanookiVerde <pedrovitor.marques@gmail.com>
2018-07-03 19:10:36 -0300

    Add Scroll nos componentes dentro de cada Slot

--- openReport/src/pdfexport/Template.java
+++ openReport/src/pdfexport/Template.java
@@ -69,11 +69,11 @@ public class Template {
     public void initialize() {
         try{
             //Adiciona Document Header
-            //Node docHeader = FXMLLoader.load(getClass().getResource(DocumentHeader.SLOT_PATH));
-            //slotGrid.getChildren().add(docHeader);
+            Node docHeader = FXMLLoader.load(getClass().getResource(DocumentHeader.SLOT_PATH));
+            slotGrid.getChildren().add(docHeader);
             //Adiciona Page Header
-           // Node pageHeader = FXMLLoader.load(getClass().getResource(PageHeader.SLOT_PATH));
-            //slotGrid.getChildren().add(pageHeader);
+            Node pageHeader = FXMLLoader.load(getClass().getResource(PageHeader.SLOT_PATH));
+            slotGrid.getChildren().add(pageHeader);
             //Adiciona Body
             Node body = FXMLLoader.load(getClass().getResource(Body.SLOT_PATH));
             slotGrid.getChildren().add(body);
@@ -81,8 +81,8 @@ public class Template {
             Node pageFooter = FXMLLoader.load(getClass().getResource(PageFooter.SLOT_PATH));
             slotGrid.getChildren().add(pageFooter);
             //Adiciona Document Footer
-            //Node documentFooter = FXMLLoader.load(getClass().getResource(DocumentFooter.SLOT_PATH));
-            //slotGrid.getChildren().add(documentFooter);
+            Node documentFooter = FXMLLoader.load(getClass().getResource(DocumentFooter.SLOT_PATH));
+            slotGrid.getChildren().add(documentFooter);
         }catch(IOException i){
             System.out.println(i.getMessage());
         }
